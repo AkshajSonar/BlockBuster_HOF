88ffc0b0f3253036cf041ec991d00bb92e606f05
SpectreKnight69 <devang.012006@gmail.com>
2025-04-05 18:49:40 +0530

    created login page

---
+++ vite-project/src/pages/IssuerPage.jsx
@@ -0,0 +1,4 @@
+  export default function IssuerPage() {
+    return <div className="p-6 text-center text-2xl">Issuer Dashboard</div>;
+  }
+  
---
+++ vite-project/src/pages/UserPage.jsx
@@ -0,0 +1,4 @@
+export default function UserPage() {
+    return <div className="text-center text-3xl p-6">User Dashboard</div>;
+  }
+  
